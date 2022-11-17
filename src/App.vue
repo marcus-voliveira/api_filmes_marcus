66c845025dceef9f90b9ded41081e56d6145e127
Marcus <marcus.oliveira2403@gmail.com>
2022-11-17 11:03:49 -0300

    VEFilmes 17/11

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import HeaderView from "./views/HeaderView.vue";
+export default {
+  components: { HeaderView },
+};
+</script>
+
+<template>
+  <HeaderView />
+
+  <main>
+    <div>
+      <h2>Destaques</h2>
+
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+    </div>
+
+    <div>
+      <h2>Filmes</h2>
+
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+    </div>
+
+    <div>
+      <h2>Séries</h2>
+
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+      <img src="./assets/imagem.jpg" alt="filme" />
+    </div>
+  </main>
+</template>
+
+<style>
+.container {
+  display: flex;
+}
+
+img {
+  width: 40%;
+  z-index: 2;
+  width: 300px;
+  margin: 20px;
+}
+</style>
---
+++ src/views/FilmesView.vue
@@ -0,0 +1,25 @@
+<script>
+const axios = "axios";
+
+const options = {
+  method: "GET",
+  url: "",
+  headers: {
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+    "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+</script>
+
+<template>asdasdasjpp</template>
+
+<style></style>
